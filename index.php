758cd3cde9bbecc55eadb56046884f7f1dae2dc7
alyhermon <Alinahermon11@gmail.com>
2023-01-05 01:38:03 -0400

    Adding put method and delete method

--- index.php
+++ index.php
@@ -6,10 +6,7 @@ require_once "model/contactos.php";
 header("Content-Type: application/json");
         switch($_SERVER['REQUEST_METHOD']){
             case 'GET':
-                if(isset($_GET['id'])){
-                    echo json_encode(contactos::getWhere($_GET['id']));
-                }
-                    echo json_encode(contactos::getAll());
+                    echo json_encode(contactos::getAll());                    
                 break;
             case 'POST':
                 $datos = json_decode(file_get_contents('php://input'));
@@ -27,15 +24,36 @@ header("Content-Type: application/json");
                 
                 break;
             case 'PUT':
-                $_PUT = json_decode(file_get_contents('php://input'),true);
-                $resultado["Mensaje"] = "Editar un Contactos con id" .$_GET['id'].
-                            ", Informacion a actualizar: " .json_encode($_PUT);
-                echo json_encode($resultado);
+                $datos = json_decode(file_get_contents('php://input'));
+                if($datos != NULL){
+                    if(contactos::update($datos->id, $datos->nombre, $datos->apellido, $datos->email, $datos->telefono)){
+                        http_response_code(200);
+                    }//End
+                    else{
+                        http_response_code(400);
+                    }
+                }
+                else{
+                    http_response_code(405);
+                }
+                
 
                 break;
             case 'DELETE':
-                $resultado["Mensaje"] = "Eliminar un Contactos con el id: " .$_GET['id'];
-                echo json_encode($resultado);
+                if(isset($_GET['id'])){
+                    if(contactos::delete($_GET['id'])){
+                        http_response_code(200);
+                    }
+                    else{
+                        http_response_code(400);
+                    }
+                }
+                else{
+                    http_response_code(405);
+                }
+                break;
+
+                default:
                 break;
         }
 
